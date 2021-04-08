82d1d2d79e1348f227cc5569b20d8d5247123ee0
VoXzE <sinncere520@gmail.com>
2021-04-08 14:02:41 -0400

    xx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ import AutoplayToggle from "./components/AutoplayToggle";
 
 
 // const socket = socketIOClient("http://localhost:5000/");
-const socket = socketIOClient("ec2-3-94-125-219.compute-1.amazonaws.com:5000");
+const socket = socketIOClient("https://kyle-twitchbot.herokuapp.com/");
 
 const App = () => {
   // Pulse state
