d32084b21e938aa7285e838f82668553eee0debc
VoXzE <sinncere520@gmail.com>
2021-04-07 21:54:10 -0400

    refactor(frontend): correct homepage url

---
+++ frontend/build/precache-manifest.f479a888d45d1a7cbac6860dbc048541.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8162c26ad1027b7123221beea66b3faa",
+    "url": "/kyle-clip/frontend/index.html"
+  },
+  {
+    "revision": "e04a8e4fdf8c160bb066",
+    "url": "/kyle-clip/frontend/static/css/main.967987d4.chunk.css"
+  },
+  {
+    "revision": "4987166451f375771878",
+    "url": "/kyle-clip/frontend/static/js/13.1b2acd4b.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/kyle-clip/frontend/static/js/13.1b2acd4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "171cf93c8ef51e27b3ca",
+    "url": "/kyle-clip/frontend/static/js/14.e50b4cb4.chunk.js"
+  },
+  {
+    "revision": "e04a8e4fdf8c160bb066",
+    "url": "/kyle-clip/frontend/static/js/main.11c064ea.chunk.js"
+  },
+  {
+    "revision": "8c28c64be5b89afc795f",
+    "url": "/kyle-clip/frontend/static/js/reactPlayerDailyMotion.15fa01fa.chunk.js"
+  },
+  {
+    "revision": "25f957de74b354984017",
+    "url": "/kyle-clip/frontend/static/js/reactPlayerFacebook.6f362f07.chunk.js"
+  },
+  {
+    "revision": "62fbbfd39412bff38ee3",
+    "url": "/kyle-clip/frontend/static/js/reactPlayerFilePlayer.dcf9cf3a.chunk.js"
+  },
+  {
+    "revision": "9dfe520856b8fe888c00",
+    "url": "/kyle-clip/frontend/static/js/reactPlayerMixcloud.666aec09.chunk.js"
+  },
+  {
+    "revision": "44173ba14c4fb5d500d4",
+    "url": "/kyle-clip/frontend/static/js/reactPlayerSoundCloud.709dce27.chunk.js"
+  },
+  {
+    "revision": "e2282fa0461a11603740",
+    "url": "/kyle-clip/frontend/static/js/reactPlayerStreamable.2ff8e61c.chunk.js"
+  },
+  {
+    "revision": "01d0a1b9185948b047ad",
+    "url": "/kyle-clip/frontend/static/js/reactPlayerTwitch.79fca751.chunk.js"
+  },
+  {
+    "revision": "3ad9812ccdc2b9a6bd9e",
+    "url": "/kyle-clip/frontend/static/js/reactPlayerVidyard.fa36ce31.chunk.js"
+  },
+  {
+    "revision": "dc1a7a55f779afb1e186",
+    "url": "/kyle-clip/frontend/static/js/reactPlayerVimeo.f6a8e4b8.chunk.js"
+  },
+  {
+    "revision": "52fe6c8715ff31608bb3",
+    "url": "/kyle-clip/frontend/static/js/reactPlayerWistia.e4ba8399.chunk.js"
+  },
+  {
+    "revision": "6124a67dab2ac72bcee4",
+    "url": "/kyle-clip/frontend/static/js/reactPlayerYouTube.f864ac07.chunk.js"
+  },
+  {
+    "revision": "1c4958224a04cc09ec75",
+    "url": "/kyle-clip/frontend/static/js/runtime-main.6a36ca07.js"
+  }
+]);
