cfd9d8cf71a87b7195b85603e77837d154156996
VoXzE <sinncere520@gmail.com>
2021-04-09 11:03:35 -0400

    refactor: generated new credentials

--- frontend/src/Firebase/index.ts
+++ frontend/src/Firebase/index.ts
@@ -2,12 +2,12 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-	apiKey: "AIzaSyA5PVO7p1qPr_nVrzsuXwFqySVBmSgAxqU",
-  authDomain: "vulture-84cb9.firebaseapp.com",
-  projectId: "vulture-84cb9",
-  storageBucket: "vulture-84cb9.appspot.com",
-  messagingSenderId: "738247494836",
-  appId: "1:738247494836:web:cdd28678f03fd3f6d05be1"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBAS_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_id
 })
 
 export default app
